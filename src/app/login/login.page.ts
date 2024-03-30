d4ffd251b51f6615f579787d5692ea75ed2e78bd
unknown <darlisonSilva.13@gmail.com>
2024-03-30 00:50:08 -0300

    Aplicativo ionic prova

---
+++ src/app/login/login.page.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ToastController } from '@ionic/angular';
+import { Color } from '@ionic/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.page.html',
+  styleUrls: ['./login.page.scss'],
+})
+export class LoginPage  {
+
+  public email: any;
+  public senha: any;
+
+  constructor(private toastController: ToastController, private router: Router) { }
+
+  login() {
+    if(this.email === 'darlison@gmail.com'  && this.senha === '123') {
+      this.router.navigateByUrl('/tela-inicial');
+      this.presentToast('Bem-vindo ao sistema', 'success')
+
+    } else {
+      this.presentToast('dandos inocorretos', 'danger')
+    }
+  }
+
+  async presentToast(texto: string, cor: Color) {
+    const  toast = await this.toastController.create({
+      message: texto,
+      color: cor,
+      duration: 3000
+    });
+    toast.present();
+  }
+
+}
